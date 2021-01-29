5c0e128b9effc186938b2e18f9a4637c0e9b39d7
Gregor Deretic <gregor.deretic@sky.uk>
2021-01-29 09:25:17 +0000

    account test

---
+++ test/account-test.js
@@ -0,0 +1,60 @@
+var assert = require("assert");
+
+class BankAccount
+{
+    constructor(amount)
+    {
+        this.balance = amount;
+    }
+
+    debit( amt )
+    {
+        this.balance -= amt;
+
+        return this.balance;
+    }
+
+    credit( amt )
+    {
+        this.balance += amt;
+
+        return this.balance;
+    }
+
+    queryBalance()
+    {
+        return this.balance;
+    }
+}
+
+
+describe('Is object constructed properly', function() {
+    it('Balance should be same as init value', function() {
+        // arrange...
+        var cut = new BankAccount(50);
+        var expectedResult = 50;
+
+        // act...
+        var actualResult = cut.queryBalance();
+
+        // assert...
+        assert.strictEqual(actualResult, expectedResult);
+        assert.strictEqual(cut.queryBalance(), expectedResult);
+    });
+  });
+
+describe('Is object debited properly', function() {
+    it('Balance should be reduced', function() {
+        // arrange...
+        var cut = new BankAccount(50);
+        var expectedResult = 20;
+        var debitAmount = 30;
+
+        // act...
+        var actualResult = cut.debit(debitAmount);
+
+        // assert...
+        assert.strictEqual(actualResult, expectedResult);
+        assert.strictEqual(cut.queryBalance(), expectedResult);
+    });
+});
